18f7e66480d47b56e1c252c7bab8c127a53aa7c0
Nicolaj Hartmann <nhh@softwarehuset.com>
2024-10-24 12:07:13 +0200

    fix

--- DatabaseMaintenance/DatabaseClient.cs
+++ DatabaseMaintenance/DatabaseClient.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.IO;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
 
@@ -15,7 +16,11 @@ namespace DatabaseMaintenance
 
         public DatabaseClient(string connectionString)
         {
-            _connectionString = connectionString;
+            var builder = new SqlConnectionStringBuilder(connectionString)
+            {
+                CommandTimeout = 0
+            };
+            _connectionString = builder.ConnectionString;
         }
 
         public async Task Initialize()
@@ -52,65 +57,63 @@ namespace DatabaseMaintenance
             }
         }
 
-        public async Task ExecuteIndexOptimizeProcedure(IndexOptions options, Action<string> output)
+        public async Task ExecuteIndexOptimizeProcedure(IndexOptions options, Action<string>? output = null, CancellationToken? cancellationToken = null)
         {
-            using (var connection = new SqlConnection(_connectionString))
+            cancellationToken ??= CancellationToken.None;
+
+            using var connection = new SqlConnection(_connectionString);
+            await connection.OpenAsync(cancellationToken.Value);
+
+            // Subscribe to the InfoMessage event to capture messages
+            connection.InfoMessage += (sender, e) =>
             {
-                await connection.OpenAsync();
-
-                // Subscribe to the InfoMessage event to capture messages
-                connection.InfoMessage += (sender, e) =>
+                foreach (SqlError error in e.Errors)
                 {
-                    foreach (SqlError error in e.Errors)
-                    {
-                        output(error.Message);
-                    }
-                };
-
-                using (var command = new SqlCommand("dbo.IndexOptimize", connection))
-                {
-                    command.CommandType = CommandType.StoredProcedure;
-
-                    // Set parameters based on the options object
-                    command.Parameters.AddWithValue("@Databases", options.Databases ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@FragmentationLow", options.FragmentationLow ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@FragmentationMedium", options.FragmentationMedium ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@FragmentationHigh", options.FragmentationHigh ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@FragmentationLevel1", options.FragmentationLevel1);
-                    command.Parameters.AddWithValue("@FragmentationLevel2", options.FragmentationLevel2);
-                    command.Parameters.AddWithValue("@MinNumberOfPages", options.MinNumberOfPages ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@MaxNumberOfPages", options.MaxNumberOfPages ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@SortInTempdb", options.SortInTempdb ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@MaxDOP", options.MaxDOP ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@FillFactor", options.FillFactor ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@PadIndex", options.PadIndex ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@LOBCompaction", options.LOBCompaction ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@UpdateStatistics", options.UpdateStatistics ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@OnlyModifiedStatistics", options.OnlyModifiedStatistics ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@StatisticsModificationLevel", options.StatisticsModificationLevel ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@StatisticsSample", options.StatisticsSample ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@StatisticsResample", options.StatisticsResample ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@PartitionLevel", options.PartitionLevel ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@MSShippedObjects", options.MSShippedObjects ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@Indexes", options.Indexes ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@TimeLimit", options.TimeLimit ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@Delay", options.Delay ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@WaitAtLowPriorityMaxDuration", options.WaitAtLowPriorityMaxDuration ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@WaitAtLowPriorityAbortAfterWait", options.WaitAtLowPriorityAbortAfterWait ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@Resumable", options.Resumable ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@AvailabilityGroups", options.AvailabilityGroups ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@LockTimeout", options.LockTimeout ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@LockMessageSeverity", options.LockMessageSeverity);
-                    command.Parameters.AddWithValue("@StringDelimiter", options.StringDelimiter ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@DatabaseOrder", options.DatabaseOrder ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@DatabasesInParallel", options.DatabasesInParallel ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@ExecuteAsUser", options.ExecuteAsUser ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@LogToTable", options.LogToTable ?? (object)DBNull.Value);
-                    command.Parameters.AddWithValue("@Execute", options.Execute ?? (object)DBNull.Value);
-
-                    await command.ExecuteNonQueryAsync();
+                    output?.Invoke(error.Message);
                 }
-            }
+            };
+
+            using var command = new SqlCommand("dbo.IndexOptimize", connection);
+            command.CommandType = CommandType.StoredProcedure;
+
+            // Set parameters based on the options object
+            command.Parameters.AddWithValue("@Databases", options.Databases ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@FragmentationLow", options.FragmentationLow ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@FragmentationMedium", options.FragmentationMedium ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@FragmentationHigh", options.FragmentationHigh ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@FragmentationLevel1", options.FragmentationLevel1);
+            command.Parameters.AddWithValue("@FragmentationLevel2", options.FragmentationLevel2);
+            command.Parameters.AddWithValue("@MinNumberOfPages", options.MinNumberOfPages ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@MaxNumberOfPages", options.MaxNumberOfPages ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@SortInTempdb", options.SortInTempdb ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@MaxDOP", options.MaxDOP ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@FillFactor", options.FillFactor ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@PadIndex", options.PadIndex ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@LOBCompaction", options.LOBCompaction ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@UpdateStatistics", options.UpdateStatistics ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@OnlyModifiedStatistics", options.OnlyModifiedStatistics ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@StatisticsModificationLevel", options.StatisticsModificationLevel ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@StatisticsSample", options.StatisticsSample ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@StatisticsResample", options.StatisticsResample ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@PartitionLevel", options.PartitionLevel ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@MSShippedObjects", options.MSShippedObjects ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@Indexes", options.Indexes ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@TimeLimit", options.TimeLimit ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@Delay", options.Delay ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@WaitAtLowPriorityMaxDuration", options.WaitAtLowPriorityMaxDuration ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@WaitAtLowPriorityAbortAfterWait", options.WaitAtLowPriorityAbortAfterWait ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@Resumable", options.Resumable ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@AvailabilityGroups", options.AvailabilityGroups ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@LockTimeout", options.LockTimeout ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@LockMessageSeverity", options.LockMessageSeverity);
+            command.Parameters.AddWithValue("@StringDelimiter", options.StringDelimiter ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@DatabaseOrder", options.DatabaseOrder ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@DatabasesInParallel", options.DatabasesInParallel ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@ExecuteAsUser", options.ExecuteAsUser ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@LogToTable", options.LogToTable ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("@Execute", options.Execute ?? (object)DBNull.Value);
+
+            await command.ExecuteNonQueryAsync(cancellationToken.Value);
         }
 
 
@@ -157,13 +160,11 @@ namespace DatabaseMaintenance
             foreach (var resourceName in resourceNames)
             {
                 // Filter the resource names to match your embedded scripts
-                if (resourceName.EndsWith("CommandExecute.sql") || resourceName.EndsWith("IndexOptimize.sql"))
+                if (resourceName.EndsWith(".sql"))
                 {
-                    using (var stream = assembly.GetManifestResourceStream(resourceName))
-                    using (var reader = new StreamReader(stream))
-                    {
-                        scripts.Add(reader.ReadToEnd());
-                    }
+                    using var stream = assembly.GetManifestResourceStream(resourceName);
+                    using var reader = new StreamReader(stream);
+                    scripts.Add(reader.ReadToEnd());
                 }
             }
 
